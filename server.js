8a13ff357e16f5e93709765acffea2f14ec884d6
IPMaddBuddha <37713898+IanPriceMB@users.noreply.github.com>
2018-06-14 15:10:32 -0600

    file removal try

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ var authRoute = require('./app/routes/auth.js')(app,passport);
 require('./config/passport/passport.js')(passport, models.user);
 
 //Sync Database
-models.sequelize.sync({}).then(function() {
+models.sequelize.sync().then(function() {
     console.log('Nice! Database looks fine')
     app.listen(port, function(err) {
         if (!err)
